0188bc9d5519feb50cd3cabd804dcd29749123ac
Joao-F3rnando <joao.goncalves0108@gmail.com>
2023-04-24 22:38:52 -0300

    start comunication with server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Waiter_Tech';
+
+  conectar()
+  {
+    $.get("http://localhost:3000/", function(resultado)
+    {
+      console.log(resultado)
+    })
+  }
+
+  EnviarDados(valor: string)
+  {
+    const json = {"dado": valor}
+    $.post("http://localhost:3000/dado",
+    json,
+    function(msg)
+    {
+      console.log(msg)
+    })
+  }
 }
